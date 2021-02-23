2a0bd08aebe68df9e921549e4ebfbb8bdf4cb6b2
make-a-mark <markaandal@gmail.com>
2021-02-23 15:36:15 -0800

    Added hw problems

---
+++ week-4/hw_problems.py
@@ -0,0 +1,33 @@
+#Question 1: Cut the following word in half in whichever way you want.
+full_word = "DVS Coding 101"
+first_half = ...
+second_half = ...
+
+#Question 2: Check that first_half and second_half are the same length (Use an operation, it should return true)
+check_len = ...
+
+#Question 3: Find the first occurence of the letter m and remove it. If you want to challenge yourself, do this in one line of code.
+word = "Lemon"
+new_word = ...
+
+#Question 4: Change the fourth letter of this word to an "h" using reassignment and indexing. (You should not be creating a new variable)
+our_word = "Pytpon"
+...
+
+#Question 5: Challenge Question: Search the "replace" function and use it to repalce all the l's with w's.
+before_replacement = "Hello World"
+after_replacement = ...
+
+#Question 6: Assign the string "weather" to the variable word_1 using string slicing.
+phrase_1 = "How's the weather today?"
+word_1 = ...
+
+#Question 7: Fill and fix the list which have the days of the week in it.
+#Take as many lines as you need.
+days = ["Monday", "Tuesday", "Wednesday", "Fridank", "racecar", "Saturday"]
+...
+...
+
+
+#Question 8: What is the largest index of days after being fixed? (Use functions/methods and arithmetic to calculate this)
+largest_index = ...#_____ (+/-/*) ____
