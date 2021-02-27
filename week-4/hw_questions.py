8d9c94c5f6ced1e0296cac0b0a10e4b03c830fe2
mgo518 <48458619+mgo518@users.noreply.github.com>
2021-02-27 13:39:19 -0600

    Add files via upload
    hw 3, 4, and 5 fixed.

---
+++ week-4/hw_questions.py
@@ -0,0 +1,35 @@
+#Question 1: Cut the following word in half in whichever way you want.
+full_word = "DVS Coding 101"
+first_half = ...
+second_half = ...
+
+#Question 2: Check that first_half and second_half are the same length (Use an operation, it should return true)
+check_len = ...
+
+#Question 3: Use the "replace" string method to turn the word "Lemon" into "Leon" (https://www.tutorialspoint.com/python/string_replace.htm)
+word = "Lemon"
+new_word = ...
+print(new_word)
+
+#Question 4: Use the "replace" string method to correct our_word to the string "Python" (there are multiple ways to do this)
+our_word = "Pytpon"
+fixed_word = ...
+
+
+#Question 5: Count the number of "l"s and "o"s combined
+count_lo = "Hello World"
+total_lo = ...
+
+#Question 6: Assign the string "weather" to the variable word_1 using string slicing.
+phrase_1 = "How's the weather today?"
+word_1 = ...
+
+#Question 7: Fill and fix the list which have the days of the week in it.
+#Take as many lines as you need.
+days = ["Monday", "Tuesday", "Wednesday", "Fridank", "racecar", "Saturday"]
+...
+
+
+
+#Question 8: What is the largest index of days after being fixed? (Use functions/methods and arithmetic to calculate this)
+largest_index = ...
