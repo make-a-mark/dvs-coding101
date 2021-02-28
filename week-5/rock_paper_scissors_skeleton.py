8906d8f8d3b1a6b691ed15af16df7e0130b234c0
make-a-mark <markaandal@gmail.com>
2021-02-27 16:39:40 -0800

    Added rock paper scissors code

---
+++ week-5/rock_paper_scissors_skeleton.py
@@ -0,0 +1,33 @@
+# STEP 1: We need to import the random library so we can have the computer randomly choose rock, paper, scissors import the library here!
+
+'''
+import ... ?
+'''
+
+
+# STEP 2: Then we want to have the user select their chose. What function did we use in Doctor Doomsday's Decimal Decider to do so? Hint: starts with an 'i'
+
+''''
+user_action = # have the user select here
+'''
+
+# STEP 3: Let's have the computer choose their action here. Hmmm. How do we make a random choice between possible strings? Hint: Maybe make a list and then define a variable to choose from that list?
+
+''''
+possible_actions = 
+computer_action = 
+'''
+
+# STEP 4: Print out what the user chose and what the computer chose!
+
+
+# STEP 5: CONDITIONAL LOGIC! We know the rules of rock, paper, scissors, now we need to implement it! Use conditionals  to compare what the user selected and what the computer selected. i.e. what happens if they chose the same thing? or if the user selected rock and the computer chose scissors? Hint: Do this in one main if-elif-elif...-else conditional block!
+
+'''
+if user_action == ""
+  ...
+elif
+  ...
+else
+  ...
+'''
